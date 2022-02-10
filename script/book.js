71f94931a911bb588bce4f851e53a72d8700974e
Hyun Oh <hoh@amazon.com>
2022-02-10 10:52:12 -0800

    JavaScript / read functionality

---
+++ script/book.js
@@ -0,0 +1,24 @@
+// const ENDPOINT = 'data/book-title-1.json';
+const ENDPOINT = 'data/';
+
+const generateURL = () => {
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get('id');
+    const language = params.get('language');
+    console.log(ENDPOINT + '?id=' + id + '&language=' + language);
+    return ENDPOINT + '/' + id + '-' + language + '.json';
+};
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', generateURL());
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var book = JSON.parse(this.responseText);
+
+        document.getElementById('book-title').innerText = book['title'];
+        document.getElementById('book-author').innerText = book['author'];
+        document.getElementById('book-text').innerText = book['text'];
+    }
+};
+xhr.send();
+
---
+++ script/index.js
@@ -0,0 +1,28 @@
+const renderBooks = (books) => {
+    for (let i = 0; i < books.length; i += 1) {
+        const book = books[i];
+        const div = document.createElement('div');
+        div.innerHTML = '<div href="#" class="list-group-item list-group-item-action flex-column align-items-start">'
+            + '<div class="d-flex w-100 justify-content-between"><h5 class="mb-1 book-title"></h5>'
+            + '<small class="book-author"></small></div><br>'
+            + '<a class="english" href="#">English</a> | <a class="spanish" href="#">Espa&ntilde;ol</a></div>';
+        div.getElementsByClassName('book-title')[0].innerHTML = book['title'];
+        div.getElementsByClassName('book-author')[0].innerHTML = book['author'];
+        div.getElementsByClassName('english')[0].setAttribute('href', 'book.html?id=' + book['id'] + '&language=english');
+        div.getElementsByClassName('spanish')[0].setAttribute('href', 'book.html?id=' + book['id'] + '&language=spanish');
+
+        document.getElementById('translated-books').appendChild(div.firstChild);
+    }
+};
+
+const ENDPOINT = 'data/translated-books.json';
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', ENDPOINT);
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var translatedBooks = JSON.parse(this.responseText);
+        renderBooks(translatedBooks);
+    }
+};
+xhr.send();
